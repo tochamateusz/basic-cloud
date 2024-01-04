80fa3df8e489223aa89dedc55610797ca6dda6fd
Tocha Mateusz <tocha.mateusz+main@gmail.com>
2024-01-04 10:24:19 +0100

    feat: check domain

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
@@ -13,6 +14,8 @@ const Version = "v1"
 type ApiRouter *gin.RouterGroup
 
 func Api(r *gin.Engine, l *zerolog.Logger) ApiRouter {
+	domain, _ := os.Hostname()
+	l.Info().Msgf("Domain: %s", domain)
 	api := r.Group(fmt.Sprintf("%s/%s/", API, Version))
 	api.GET("/", func(ctx *gin.Context) {
 		l.Debug().Msg("test2")
